3dd6bab8ef6c52a509031339ce5087706ac38029
Yadav <2212938@cognizant.com>
2024-08-10 17:26:25 +0530

    fix build error

--- components/products/FavoriteToggleButton.tsx
+++ components/products/FavoriteToggleButton.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { FaHeart } from "react-icons/fa";
 import { Button } from "../ui/button";
 
-function FavoriteToggleButton() {
+function FavoriteToggleButton({ productId }: { productId: string }) {
   return (
     <Button size="icon" variant="outline" className="p-2 cursor-pointer">
       <FaHeart />

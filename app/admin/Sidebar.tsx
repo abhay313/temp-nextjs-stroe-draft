b08c6d382dd7ea505646ff30ede66c79b3cd6819
Yadav <2212938@cognizant.com>
2024-08-26 11:15:20 +0530

    bug fixing

--- app/admin/Sidebar.tsx
+++ app/admin/Sidebar.tsx
@@ -18,7 +18,9 @@ function Sidebar() {
             className="w-full mb-2 capitalize font-normal justify-start"
             variant={variant}
           >
-            <Link href={link.href}>{link.label}</Link>
+            <Link key={link.href} href={link.href}>
+              {link.label}
+            </Link>
           </Button>
         );
       })}

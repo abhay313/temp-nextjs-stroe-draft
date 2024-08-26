6de707d5e7ba953d75ea3e1254dba7485fcdc113
Yadav <2212938@cognizant.com>
2024-08-26 11:08:07 +0530

    added CRUD of products and fav functionality

---
+++ app/admin/Sidebar.tsx
@@ -0,0 +1,29 @@
+"use client";
+import { adminLinks } from "@/utils/links";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { Button } from "@/components/ui/button";
+
+function Sidebar() {
+  const pathname = usePathname();
+
+  return (
+    <aside>
+      {adminLinks.map((link) => {
+        const isActiveLink = pathname === link.href;
+        const variant = isActiveLink ? "default" : "ghost";
+        return (
+          <Button
+            asChild
+            className="w-full mb-2 capitalize font-normal justify-start"
+            variant={variant}
+          >
+            <Link href={link.href}>{link.label}</Link>
+          </Button>
+        );
+      })}
+    </aside>
+  );
+}
+
+export default Sidebar;

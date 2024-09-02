4ee8315660a23f4515ba891dd3c2ccc03e7bf7c6
Yadav <2212938@cognizant.com>
2024-09-02 10:48:50 +0530

    added cart, orders and payment functionality

--- utils/schemas.ts
+++ utils/schemas.ts
@@ -24,7 +24,6 @@ export const productSchema = z.object({
     }
   ),
 });
-
 export const imageSchema = z.object({
   image: validateImageFile(),
 });
@@ -36,7 +35,7 @@ function validateImageFile() {
     .instanceof(File)
     .refine((file) => {
       return !file || file.size <= maxUploadSize;
-    }, `File size must be less than 1 MB`)
+    }, "File size must be less than 1MB")
     .refine((file) => {
       return (
         !file || acceptedFileTypes.some((type) => file.type.startsWith(type))
@@ -44,16 +43,35 @@ function validateImageFile() {
     }, "File must be an image");
 }
 
-export function validatedWithZodSchema<T>(
+export function validateWithZodSchema<T>(
   schema: ZodSchema<T>,
   data: unknown
 ): T {
   const result = schema.safeParse(data);
-
   if (!result.success) {
     const errors = result.error.errors.map((error) => error.message);
     throw new Error(errors.join(","));
   }
-
   return result.data;
 }
+
+export const reviewSchema = z.object({
+  productId: z.string().refine((value) => value !== "", {
+    message: "Product ID cannot be empty",
+  }),
+  authorName: z.string().refine((value) => value !== "", {
+    message: "Author name cannot be empty",
+  }),
+  authorImageUrl: z.string().refine((value) => value !== "", {
+    message: "Author image URL cannot be empty",
+  }),
+  rating: z.coerce
+    .number()
+    .int()
+    .min(1, { message: "Rating must be at least 1" })
+    .max(5, { message: "Rating must be at most 5" }),
+  comment: z
+    .string()
+    .min(10, { message: "Comment must be at least 10 characters long" })
+    .max(1000, { message: "Comment must be at most 1000 characters long" }),
+});
